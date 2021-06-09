d86863e75a0f55808c143cb6c3bcc269c7734d1a
Paul Danelli <prdanelli@gmail.com>
2021-06-09 21:52:08 +0100

    Short out polybar colours

--- polybar/shapes/scripts/launcher.sh
+++ polybar/shapes/scripts/launcher.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/shapes/scripts/rofi/launcher.rasi
+# rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/shapes/scripts/rofi/launcher.rasi
+rofi -no-lazy-grab -show drun -modi drun
