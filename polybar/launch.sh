003099e64d78b520e6b53c45b67a63cf9293a59b
Paul Danelli <prdanelli@gmail.com>
2021-07-30 10:01:15 +0100

    Nicer polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-polybar -q main -c "$HOME/.config/polybar/config.ini" &
+polybar -q left -c "$HOME/.config/polybar/config.ini" &
+
+current=$(xrandr --listactivemonitors | grep -v Monitors | grep -Eo '[^ ]+$')
+if [ $current == "DP-1" ]
+then
+	polybar -q right_external -c "$HOME/.config/polybar/config.ini" &
+else
+	polybar -q right -c "$HOME/.config/polybar/config.ini" &
+fi
 
