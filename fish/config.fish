f188c4631a2085d0c4efe5c0b3e4edf79a2de7d4
Paul Danelli <prdanelli@gmail.com>
2021-10-08 17:36:17 +0100

    Add gcloud functions

--- fish/config.fish
+++ fish/config.fish
@@ -18,7 +18,7 @@ function linux:purge_kernals:test
 end
 
 function spicetify:apply
-  command spicetify config current_theme $argv; spicetify apply
+	command spicetify config current_theme $argv; spicetify apply
 end
 
 function cclip
@@ -30,47 +30,47 @@ function mkfile
 end
 
 function unset
-    set --erase $argv
+	set --erase $argv
 end
 
 function ls
-    command exa $argv
+	command exa $argv
 end
 
 function starship:edit
-    command nvim ~/.config/starship.toml
+	command nvim ~/.config/starship.toml
 end
 
 function kitty:edit
-    command nvim ~/.config/kitty/kitty.conf
+	command nvim ~/.config/kitty/kitty.conf
 end
 
 function git:edit
-    command nvim ~/.gitconfig
+	command nvim ~/.gitconfig
 end
 
 function dc
-    command docker-compose $argv
+	command docker-compose $argv
 end
 
 function dce
-    command docker-compose exec $argv
+	command docker-compose exec $argv
 end
 
 function dce:web:bash
-    command docker-compose exec web bash
+	command docker-compose exec web bash
 end
 
 function dce:web:console
-    command docker-compose exec web bundle exec rails console
+	command docker-compose exec web bundle exec rails console
 end
 
 function dce:web:rspec
-    command docker-compose exec web bundle exec rspec $argv
+	command docker-compose exec web bundle exec rspec $argv
 end
 
 function dce:web:rubocop
-    command docker-compose exec web bundle exec rubocop -a
+	command docker-compose exec web bundle exec rubocop -a
 end
 
 function dce:web:rails
@@ -85,14 +85,26 @@ function dce:web:attach
 	command docker-compose up -d web workers; docker attach hyku_addons_web_1
 end
 
-function gcloud:deploy:ah
+function gcloud:cluster:ah
 	bash -c "gcloud config configurations activate ubiquityrepo-ah; gcloud container clusters get-credentials cluster-ah --zone europe-west4-a;"
+end
+
+function gcloud:cluster:us
+	bash -c "gcloud config configurations activate ubiquityrepo-us; gcloud container clusters get-credentials cluster-us --zone us-central1-a;"
+end
+
+function gcloud:deploy
 	bash -c "helm repo update; helm upgrade hyku ubiquity-charts-hyku/hyku"
 end
 
+function gcloud:deploy:ah
+	gcloud:cluster:ah
+	gcloud:deploy
+end
+
 function gcloud:deploy:us
-	bash -c "gcloud config configurations activate ubiquityrepo-us; gcloud container clusters get-credentials cluster-us --zone us-central1-a;"
-	bash -c "helm repo update; helm upgrade hyku ubiquity-charts-hyku/hyku"
+	gcloud:cluster:us
+	gcloud:deploy
 end
 
 function gcloud:hyku:bash
@@ -107,34 +119,40 @@ function gcloud:hyku:pods
 	kubectl get pods | grep hyku
 end
 
-function ha:rspec
-	bash -c 'bundle exec rspec `find spec -name "*_spec.rb" | grep -v internal_test_hyrax`'
+function gcloud:notes:ah
+	gcloud:cluster:ah
+	helm get notes hyku
+end
+
+function gcloud:notes:us
+	gcloud:cluster:us
+	helm get notes hyku
 end
 
 function tmux:edit
-    command nvim ~/.tmux.conf
+	command nvim ~/.tmux.conf
 end
 
 function colours
-    bash -c 'for i in {0..255}; do
-        printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
-    done'
+	bash -c 'for i in {0..255}; do
+	printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+	done'
 end
 
 function fish:history
-    nvim ~/.config/fish/fish_history
+	nvim ~/.config/fish/fish_history
 end
 
 function fish:edit
-    nvim ~/.config/fish/config.fish
+	nvim ~/.config/fish/config.fish
 end
 
 function fish:config
-    nvim ~/.config/fish/config.fish
+	nvim ~/.config/fish/config.fish
 end
 
 function fish:reload
-    source ~/.config/fish/config.fish
+	source ~/.config/fish/config.fish
 end
 
 set -gx PATH ~/Personal/dotfiles/diff-so-fancy /usr/local/bin /usr/local/go/bin ~/local/bin ~/.gem/ruby/2.7.0/bin $PATH
@@ -146,7 +164,7 @@ set fish_greeting ""
 
 # Ensure we start the auth daemon as bspwm doesn't seem to do this automatically
 if test -n "$DESKTOP_SESSION"
-    set -x (gnome-keyring-daemon --start | string split "=")
+	set -x (gnome-keyring-daemon --start | string split "=")
 end
 
 # Set Bat theme, which is used for Vim FZF previews
