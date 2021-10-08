10d68df3c14ff3142a771ce5bb34879b455730cd
Paul Danelli <prdanelli@gmail.com>
2021-10-08 17:35:46 +0100

    Get backup working correctly

--- duplicity/backup.sh
+++ duplicity/backup.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
 
-notify-send "Backup" "Starting remote backup"
+# Send notify messages when in a cronjob
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-# + Setup OAth Credentials in Google API
-#
-# https://rgarth.github.io/2017/10/29/Grive-and-Duplicity/
-# + Go to https://console.developers.google.com/
-# + Click “Create Project”
-# + Give it some sensible name like “Duplicity Backup”
-# + From the Dashboard click on “Enable API”
-# + Search for “Drive”, select the Drive API, and enable it
-# + You will now need to create some credentials, Select “Credentials” from the panel on the left
-# + Use the arrow on the Create Credentials button to select “OAuth Client ID”
-# + You will need to give a Product Name. I used “Duplicity Backup”
-# + For Application Type, select “Other” and name the client. I used “Duplicity”
-# + Save your credentials somewhere safe (although they can easily be regenerated)
-# + Download client_secrets.json from GCloud and put it in the directory
+# Is this is used as a CRON $HOME wont be set
+TARGET=/home/paul
+
+notify-send "Backup" "Starting remote backup of $TARGET"
 
 # USE A GOOD LONG PASSPHRASE! DO NOT LOSE IT!
-export PASSPHRASE=$(cat ~/.duplicity/passphrase | tr -d ' ') # String used for encryption
-USERNAME=$(cat ~/.duplicity/username | tr -d ' ') # Username from email address
+export PASSPHRASE=$(cat ~/.duplicity/passphrase.conf | tr -d ' ')
 
 # 3 is a good verbosity level for daily runs, but you may want to start with
 # 5 or above if you are just setting up
-VERBOSITY=0
+VERBOSITY=5
 
 # If duplicity finds this file in any folder, that folder won't be backed up.
 EXCLUDE_IF_PRESENT=".noback"
 
 # Read the manpage for the exclude filelist format. You can specify
-EXCLUDE_FILELIST=$HOME/.duplicity/excludes
+EXCLUDE_FILELIST=$TARGET/.duplicity/excludes.conf
 
-# Backup configuration settings
-# Set this to point to your Google Drive location
-DEST="gdocs://$USERNAME@gmail.com/duplicity"
+# Backup location with OS prepended
+DEST="pydrive+gdocs://developer.gserviceaccount.com/$(uname -n)_duplicity_backup"
 
 # You can specify a different offsite-specific exclude filelist here
 EXCLUDE_FILELIST=$EXCLUDE_FILELIST
@@ -43,18 +32,16 @@ EXCLUDE_IF_PRESENT=$EXCLUDE_IF_PRESENT
 
 # You should print out the contents of this file, in case you
 # need to restore from off-site backups after a major disaster.
-export GOOGLE_DRIVE_SETTINGS=$HOME/.duplicity/credentials
+export GOOGLE_DRIVE_SETTINGS=$TARGET/.duplicity/pydrive.yaml
 
 duplicity \
 	--verbosity=$VERBOSITY \
 	--allow-source-mismatch \
 	--exclude-if-present=$EXCLUDE_IF_PRESENT \
 	--exclude-filelist=$EXCLUDE_FILELIST \
-	$HOME ${DEST}
-
-# Cleanup - Remove all backed up files
-# duplicity --verbosity=0 cleanup --force gdocs://prwhitehead@gmail.com/duplicity
+	$TARGET ${DEST}
 
 unset GOOGLE_DRIVE_SETTINGS
+unset PASSPHRASE
 
 notify-send "Backup" "Finished remote backup"
