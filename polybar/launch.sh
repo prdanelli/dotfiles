3c2503d789ce73ad7085e384b4f369113009b9e0
Paul Danelli <prdanelli@gmail.com>
2021-12-30 14:33:36 +0000

    Remove the second monitor polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,11 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-current=$(xrandr --listactivemonitors | grep -v Monitors | grep -Eo '[^ ]+$')
-if [ $current == "DP-1" ]
-then
-	polybar -q main_external -c "$HOME/.config/polybar/config.ini" &
-else
-	polybar -q main -c "$HOME/.config/polybar/config.ini" &
-fi
+polybar -q main -c "$HOME/.config/polybar/config.ini" &
 
