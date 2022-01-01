7a334d48ec0b81d0205efea6399a59dc37f500bb
Paul Danelli <prdanelli@gmail.com>
2022-01-01 21:58:50 +0000

    Setup keybindings for bufferline tabs

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -4,6 +4,26 @@ require('bufferline').setup {
 		diagnostics_update_in_insert = false,
 		show_buffer_icons = false,
 		show_tab_indicators = false,
-		separator_style = "none"
+		separator_style = "none",
+		indicator_icon = "",
 	}
 }
+
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", opts)
+keymap("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", opts)
+keymap("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", opts)
+keymap("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", opts)
+keymap("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>", opts)
+keymap("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>", opts)
+keymap("n", "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>", opts)
+keymap("n", "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>", opts)
+keymap("n", "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>", opts)
+
+keymap("n", "tn", "<cmd>BufferLineMovePrev<CR>", opts)
+keymap("n", "tm", "<cmd>BufferLineMoveNext<CR>", opts)
+keymap("n", "te", "<cmd>BufferLineSortByExtension<CR>", opts)
+keymap("n", "td", "<cmd>BufferLineSortByDirectory<CR>", opts)
