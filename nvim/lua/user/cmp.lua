0ea36801f5e73871170bd600523d03a004ddac6d
Paul Danelli <prdanelli@gmail.com>
2022-01-03 20:47:45 +0000

    Switch ctrl p/n as they're backwards

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -51,7 +51,7 @@ cmp.setup {
 		end,
 	},
 	mapping = {
-		["<C-p>"] = cmp.mapping(function(fallback)
+		["<C-n>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expandable() then
@@ -64,7 +64,7 @@ cmp.setup {
 				fallback()
 			end
 		end, { "i", "s" }),
-		["<C-n>"] = cmp.mapping(function(fallback)
+		["<C-p>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
