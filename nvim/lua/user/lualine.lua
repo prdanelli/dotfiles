49aa57646b74f503ff8e3cbb43d9267aabee0a00
Paul Danelli <prdanelli@gmail.com>
2022-01-03 20:48:27 +0000

    Tweak configs and add return

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,27 +1,33 @@
-require'lualine'.setup({
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+
+lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = 'auto',
-		component_separators = { left = '\\', right = '/' },
-		section_separators = { left = '', right = '' },
-		disabled_filetypes = {}
+		theme = "auto",
+		component_separators = { left = "\\", right = "/" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree", "alpha" }
 		},
 	sections = {
-		lualine_a = { 'mode' },
-		lualine_b = { 'branch' },
-		lualine_c = { require'lsp-status'.status },
-		lualine_x = { 'filename', 'filetype' },
-		lualine_y = { 'progress' },
-		lualine_z = { 'location' }
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = { require("lsp-status").status },
+		lualine_x = { "filename", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" }
 		},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { 'filename' },
-		lualine_x = { 'location' },
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {}
 		},
 	tabline = {},
 	extensions = {}
 })
+
--- nvim/lua/user/togglebool.lua
+++ nvim/lua/user/togglebool.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<leader>t", "<cmd>ToggleBool<cr>", opts)
+keymap("n", "<C-t>", "<cmd>ToggleBool<cr>", opts)
 
