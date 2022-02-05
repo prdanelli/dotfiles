24ab34db60634ed334b3545f84ee2cc04f1ec3e7
Paul Danelli <prdanelli@gmail.com>
2022-02-05 17:01:24 +0000

    Disable CMP inside comments

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -130,6 +130,14 @@ cmp.setup {
 		ghost_text = false,
 		native_menu = false,
 	},
+	enabled = function()
+		local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+		if in_prompt then  -- this will disable cmp in the Telescope window (taken from the default config)
+			return false
+		end
+		local context = require("cmp.config.context")
+		return not(context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+	end
 }
 
 vim.cmd([[
