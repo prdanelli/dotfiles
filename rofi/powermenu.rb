07521ed3057f8d1c7fff64aa321f62eef13d509a
Paul Danelli <prdanelli@gmail.com>
2022-03-13 19:41:54 +0000

    Use ruby for powermenu - thanks to Fuzz Gem

---
+++ rofi/powermenu.rb
@@ -0,0 +1,26 @@
+#!/home/paul/.rvm/rubies/ruby-3.0.3/bin/ruby
+
+# The Gem doesn't allow us the option of passing custom themes as arguments
+require_relative "fuzz/rofi_picker.rb"
+
+rofi_args = {
+  "theme" => "~/.config/rofi/powermenu.rasi"
+}
+
+options = {
+  suspend: { text: "", command: "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause; systemctl suspend" },
+  shutdown: { text: "", command: "systemctl poweroff" },
+  reboot: { text: "", command: "systemctl reboot" },
+  logout: { text: "", command: "bspc quit" },
+}
+
+values = options.values.map { |hsh| hsh[:text] }
+selector = Fuzz::Selector.new(values, picker: Fuzz::RofiPicker.new(rofi_args))
+result = selector.pick
+
+selected = options.find { |k,v| v[:text] == result }
+
+return if selected.nil?
+
+`#{selected.dig(1, :command)}`
+
