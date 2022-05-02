8387d0593ba81e99f42e5d511289a41ee16edfea
Paul Danelli <prdanelli@gmail.com>
2022-05-02 21:50:54 +0100

    Fix issue with vim global not found

--- nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -2,12 +2,14 @@ return {
   settings = {
     Lua = {
       diagnostics = {
+        -- Get the language server to recognize the `vim` global
         globals = { "vim" },
       },
       workspace = {
+        -- Make the server aware of Neovim runtime files
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
         },
       },
     },
