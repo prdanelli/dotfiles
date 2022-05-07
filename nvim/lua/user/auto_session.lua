e12b53b7452311a9d6a03af6d7d58e3be47047a7
Paul Danelli <prdanelli@gmail.com>
2022-05-07 15:50:32 +0100

    Try and disable sessions for some internal filetypes

--- nvim/lua/user/auto_session.lua
+++ nvim/lua/user/auto_session.lua
@@ -1,6 +1,6 @@
-local status_ok, bufferline = pcall(require, "auto-session")
+local status_ok, _ = pcall(require, "auto-session")
 if not status_ok then
   return
 end
 
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+vim.o.sessionoptions="blank,buffers,curdir,folds,tabpages,winsize,winpos"
