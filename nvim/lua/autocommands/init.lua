ad961fb9d96d07549beabb200b5b371a20370568
Paul Danelli <prdanelli@gmail.com>
2022-05-15 21:51:39 +0100

    Remove old auto command for setting telescope highlight

--- nvim/lua/autocommands/init.lua
+++ nvim/lua/autocommands/init.lua
@@ -1,7 +1,6 @@
 load "autocommands.general"
 load "autocommands.ruby"
 load "autocommands.editing"
-load "autocommands.color_scheme"
 load "autocommands.git"
 load "autocommands.formatting"
 load "autocommands.alpha"
