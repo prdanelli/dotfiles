cb99f6b31b165fcaac1fee5c8a02e9a8461503ac
Paul Danelli <prdanelli@gmail.com>
2022-05-20 10:26:30 +0100

    Sorting buffers

--- nvim/lua/plugins/config/bufferline.lua
+++ nvim/lua/plugins/config/bufferline.lua
@@ -20,7 +20,7 @@ bufferline.setup({
     separator_style = "none",
     indicator_icon = "│",
     offsets = { { filetype = "NvimTree", text = "" } },
-    sort_by = "id",
+    sort_by = "insert_after_current",
   },
 })
 
