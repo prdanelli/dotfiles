d81d5309335424fb3c42080347b59baeba8c2312
Paul Danelli <prdanelli@gmail.com>
2022-05-20 22:03:25 +0100

    Tidy up

--- zsh/user/completions.sh
+++ zsh/user/completions.sh
@@ -2,17 +2,17 @@
 # zsh_add_completion "esc/conda-zsh-completion" false
 #
 function zsh_add_completion() {
-    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
-        # For completions
-		completion_file_path=$(ls $ZDOTDIR/plugins/$PLUGIN_NAME/_*)
-		fpath+="$(dirname "${completion_file_path}")"
-        zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh"
-    else
-        git clone "https://github.com/$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
-		fpath+=$(ls $ZDOTDIR/plugins/$PLUGIN_NAME/_*)
-        [ -f $ZDOTDIR/.zccompdump ] && $ZDOTDIR/.zccompdump
-    fi
-	completion_file="$(basename "${completion_file_path}")"
-	if [ "$2" = true ] && compinit "${completion_file:1}"
+  PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
+  if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
+    # For completions
+    completion_file_path=$(ls $ZDOTDIR/plugins/$PLUGIN_NAME/_*)
+    fpath+="$(dirname "${completion_file_path}")"
+    zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh"
+  else
+    git clone "https://github.com/$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
+    fpath+=$(ls $ZDOTDIR/plugins/$PLUGIN_NAME/_*)
+    [ -f $ZDOTDIR/.zccompdump ] && $ZDOTDIR/.zccompdump
+  fi
+  completion_file="$(basename "${completion_file_path}")"
+  if [ "$2" = true ] && compinit "${completion_file:1}"
 }
--- zsh/user/packages.sh
+++ zsh/user/packages.sh
@@ -20,9 +20,7 @@ function zsh_add_plugin() {
   PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
 
   if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
-    # For plugins
-    zsh_add_config "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-      zsh_add_config "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
+    zsh_add_config "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || zsh_add_config "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
   else
     git clone "https://github.com/$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
   fi
