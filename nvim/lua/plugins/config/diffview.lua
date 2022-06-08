598d4ba72fe4d22f249f063a0f03d378ae1b4f81
Paul Danelli <prdanelli@gmail.com>
2022-06-08 19:05:55 +0100

    Diffview deprecations

--- nvim/lua/plugins/config/diffview.lua
+++ nvim/lua/plugins/config/diffview.lua
@@ -24,16 +24,6 @@ diffview.setup {
       folder_statuses = "only_folded",  -- One of 'never', 'only_folded' or 'always'.
     },
   },
-  file_history_panel = {
-    log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
-    },
-  },
   default_args = {    -- Default args prepended to the arg-list for the listed commands
     DiffviewOpen = {},
     DiffviewFileHistory = {},
