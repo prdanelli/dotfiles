402a61ae1ff87a54887edcf50a0981ffd402273c
Paul Danelli <prdanelli@gmail.com>
2022-06-15 09:21:13 +0100

    Disable core features to speed loading times

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -60,3 +60,29 @@ vim.cmd([[
   set fillchars=fold:-
 ]])
 
+-- Disable some builtin vim plugins
+local disabled_built_ins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "matchparen",
+  "tar",
+  "tarPlugin",
+  "tutor",
+  "rrhelper",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  vim.g["loaded_" .. plugin] = 1
+end
