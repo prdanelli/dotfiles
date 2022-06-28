cc05e47584734066a1cb525201ad68caee3cbc7c
Paul Danelli <prdanelli@gmail.com>
2022-06-28 21:47:42 +0100

    Tweak which key options

--- nvim/lua/plugins/config/whichkey.lua
+++ nvim/lua/plugins/config/whichkey.lua
@@ -5,7 +5,21 @@ function M.setup()
   local whichkey = require(module_name)
 
   local conf = {
+    show_help = false,
     ignore_missing = true,
+    plugins = {
+      marks = false,
+      registers = false,
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false, -- help for text objects triggered after entering an operator
+        windows = false, -- default bindings on <c-w>
+        nav = true, -- misc bindings to work with windows
+        z = false, -- bindings for folds, spelling and others prefixed with z
+        g = false, -- bindings for prefixed with g
+      },
+    },
     window = {
       border = "single", -- none, single, double, shadow
       position = "bottom", -- bottom, top
