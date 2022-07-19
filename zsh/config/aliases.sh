f674db1e0be2da04b49d319ae26520fb96e75dbf
Paul Danelli <prdanelli@gmail.com>
2022-07-19 19:28:00 +0100

    Add ZSH aliases

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -27,3 +27,9 @@ alias t="tmux"
 alias ta="t a -t"
 alias tls="t ls"
 alias tn="t new -t"
+
+# Olio
+alias olio:storage="zsh ~/Dev/storage.sh"
+
+# Yabai
+alias yabai:restart="brew services restart yabai"
