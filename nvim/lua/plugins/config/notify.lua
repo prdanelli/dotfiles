84608f9f5a420ef87a4997bb8bfe98e8e10a92dd
Paul Danelli <prdanelli@gmail.com>
2022-10-12 10:45:12 +0100

    Remove notify

--- nvim/lua/plugins/config/notify.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, notify = pcall(require, "notify")
-if not status_ok then return end
-
-notify.setup({
-  stages = "fade",
-  background_colour = 'FloatShadow',
-  timeout = 2000,
-})
