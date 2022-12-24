80a88cbbc3502c744ff6d42a147a336ca7438125
Paul Danelli <prdanelli@gmail.com>
2022-12-24 10:45:05 +0000

    Require plugins and lsp as safe load doesn't work

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,11 +4,11 @@ if impatient_loaded then
 end
 
 require "utils"
+require "plugins" -- Errors are generated with safe_load
+require "lsp"
 
 safe_load "config.options"
 safe_load "config.keymaps"
 safe_load "config.disable_builtins"
-safe_load "lsp"
-safe_load "plugins"
 safe_load "autocommands"
 safe_load "user.invert_term"
