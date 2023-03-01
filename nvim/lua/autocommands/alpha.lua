f1463c29795220290649fcaa84a8f2db3ca19839
Paul Danelli <prdanelli@gmail.com>
2023-03-01 21:01:56 +0000

    Fix Alpha cosmetic autocommands

--- nvim/lua/autocommands/alpha.lua
+++ nvim/lua/autocommands/alpha.lua
@@ -1,17 +1,11 @@
 local plugin_loaded, alpha = pcall(require, "alpha")
 if not plugin_loaded then return end
 
--- local plugin_loaded, alpha = pcall(require, "indentlines")
--- if not plugin_loaded then return end
---
--- vim.api.nvim_create_augroup("_alpha", { clear = true })
---
--- -- Alpha Specific fixes to adjust the display
--- vim.api.nvim_create_autocmd("User", {
---   group = "_alpha",
---   pattern = "AlphaReady",
---   callback = function()
---     vim.cmd("set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2")
---     vim.cmd("IndentLinesToggle")
---   end,
--- })
+vim.api.nvim_create_augroup("_alpha", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+  group = "_alpha",
+  pattern = "AlphaReady",
+  callback = function()
+    vim.cmd("set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2")
+  end,
+})
