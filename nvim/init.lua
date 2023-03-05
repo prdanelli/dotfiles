3a4c1a65138ce3e06db7b3148f9a20d4447ecef5
Paul Danelli <prdanelli@gmail.com>
2023-03-05 18:01:46 +0000

    Use require

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,8 @@ require "utils"
 require "plugins" -- Errors are generated with safe_load
 require "lsp"
 
-safe_load "config.options"
-safe_load "config.keymaps"
-safe_load "config.disable_builtins"
-safe_load "autocommands"
-safe_load "user.invert_term"
+require "config.options"
+require "config.keymaps"
+require "config.disable_builtins"
+require "autocommands"
+require "user.invert_term"
