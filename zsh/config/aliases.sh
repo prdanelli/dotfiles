37c5e5c37dc605613b3ce7d8324660d4507acf87
Paul Danelli <prdanelli@gmail.com>
2023-04-26 10:58:25 +0100

    Add Docker aliases

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -36,6 +36,8 @@ alias olio:stop_storage="docker stop redis-dev mysql-dev"
 
 # Yabai
 alias yabai:reload="brew services restart yabai"
+alias yabai:start="brew services start yabai"
+alias yabai:stop="brew services stop yabai"
 alias yabai:install_sa="sudo yabai --load-sa"
 alias yabai:borders:off="yabai -m config window_border off"
 alias yabai:borders:on="yabai -m config window_border on"
@@ -45,9 +47,17 @@ alias skhd:keys="bat ~/.config/skhd/skhdrc"
 alias skhd:reload="brew services restart skhd"
 
 # Brew
-alias brew:upgrade:all="brew upgrade; brew remove nvim; brew install --HEAD nvim; nvim --headless '+Lazy! sync' +qa;"
-# alias brew:upgrade:all="brew upgrade"
+# alias brew:upgrade:all="brew upgrade; brew remove nvim; brew install --HEAD nvim; nvim --headless '+Lazy! sync' +qa;"
+alias brew:upgrade:all="brew upgrade; nvim --headless '+Lazy! sync' +qa;"
 alias brewbundle="brew bundle --file ~/.Brewfile"
 
 # Docker Compose
 alias dc="docker-compose"
+alias dcr="docker-compose run --rm"
+function docker-attach() {
+  docker attach $(docker-compose ps -q $1)
+}
+
+
+# ASDF
+alias asdf:versions="nvim /Users/paul/.tool-versions"
