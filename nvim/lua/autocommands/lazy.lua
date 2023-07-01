af7191fae7fd70d347cb8f62fd4790473c095ca0
Paul Danelli <prdanelli@gmail.com>
2023-07-01 18:56:01 +0100

    Remove some autocommands

--- nvim/lua/autocommands/lazy.lua
+++ nvim/lua/autocommands/lazy.lua
@@ -7,18 +7,9 @@ vim.api.nvim_create_autocmd("FileType", {
   group = "_lazy",
   pattern = "lazy",
   callback = function()
-    vim.cmd("set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2")
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = "_lazy",
-  pattern = "lazy",
-  callback = function()
     vim.cmd("setlocal nonumber colorcolumn= | autocmd BufUnload set colorcolumn=80,120")
   end,
 })
-
 
 local colorizer_loaded, _ = pcall(require, "colorizer")
 if colorizer_loaded then
