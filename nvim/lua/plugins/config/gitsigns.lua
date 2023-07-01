595aac3075fa6df014187391a569bc5b8c695b25
Paul Danelli <prdanelli@gmail.com>
2023-07-01 18:57:56 +0100

    Remove deprecated keymaps

--- nvim/lua/plugins/config/gitsigns.lua
+++ nvim/lua/plugins/config/gitsigns.lua
@@ -17,27 +17,6 @@ gitsigns.setup {
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  keymaps = {
-    noremap = true,
-
-    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'"},
-    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
-
-    ["n <leader>hs"] = "<cmd>Gitsigns stage_hunk<CR>",
-    ["v <leader>hs"] = "<cmd>Gitsigns stage_hunk<CR>",
-    ["n <leader>hu"] = "<cmd>Gitsigns undo_stage_hunk<CR>",
-    ["n <leader>hr"] = "<cmd>Gitsigns reset_hunk<CR>",
-    ["v <leader>hr"] = "<cmd>Gitsigns reset_hunk<CR>",
-    ["n <leader>hR"] = "<cmd>Gitsigns reset_buffer<CR>",
-    ["n <leader>hp"] = "<cmd>Gitsigns preview_hunk<CR>",
-    ["n <leader>hb"] = "<cmd>lua require('gitsigns').blame_line({ full = true })<CR>",
-    ["n <leader>hS"] = "<cmd>Gitsigns stage_buffer<CR>",
-    ["n <leader>hU"] = "<cmd>Gitsigns reset_buffer_index<CR>",
-
-    -- Text objects
-    ["o ih"] = ":<C-U>Gitsigns select_hunk<CR>",
-    ["x ih"] = ":<C-U>Gitsigns select_hunk<CR>"
-  },
   watch_gitdir = {
     interval = 1000,
     follow_files = true
