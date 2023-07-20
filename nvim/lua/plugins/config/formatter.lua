fdfd03d6fa37bfe92292636648993d097f13a461
Paul Danelli <prdanelli@gmail.com>
2023-07-20 22:02:13 +0100

    Add formatter

---
+++ nvim/lua/plugins/config/formatter.lua
@@ -0,0 +1,45 @@
+local loaded, formatter = pcall(require, "formatter")
+if not loaded then
+  return
+end
+
+local util = require("formatter.util")
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("_formatter", { clear = true }),
+  pattern = "*",
+  command = "FormatWrite",
+})
+
+-- Provides the following commands:
+-- Format, FormatWrite, FormatLock, FormatWriteLock
+formatter.setup({
+  logging = true,
+  log_level = vim.log.levels.WARN,
+  filetype = {
+    ruby = {
+      -- require("formatter.filetypes.ruby").rubocop,
+      function()
+        return {
+          exe = "rubocop",
+          args = {
+            "--fix-layout",
+            "--autocorrect-all",
+            "--stdin",
+            util.escape_path(util.get_current_buffer_file_name()),
+            "--format",
+            "files",
+            "--stderr",
+          },
+          stdin = true,
+        }
+      end,
+    },
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
+})
