fe0b4d3234ee5b2c810da583792f2d9bac631ec1
Paul Danelli <prdanelli@gmail.com>
2023-07-20 22:14:01 +0100

    Remove autocommand

--- nvim/lua/autocommands/formatting.lua
+++ nvim/lua/autocommands/formatting.lua
@@ -1,14 +1,8 @@
 vim.api.nvim_create_augroup("_formatting", { clear = true })
 
--- Strip trailing whitespaces on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = "_formatting",
-  pattern = "*",
-  command = "%s/\\s\\+$//e"
-})
-
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })
