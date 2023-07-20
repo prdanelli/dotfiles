93647c0288a23357060ac44b3eb1254e1c4f1501
Paul Danelli <prdanelli@gmail.com>
2023-07-20 22:03:19 +0100

    Config Rnvimr to be bigger

--- nvim/lua/plugins/config/rnvimr.lua
+++ nvim/lua/plugins/config/rnvimr.lua
@@ -1,4 +1,3 @@
-local colors = require("config.colors").colors
 local options = {
   rnvimr_enable_ex = 1, -- Make Ranger replace netrw and be the file explorer
   rnvimr_enable_picker = 1, -- Make Ranger to be hidden after picking a file
@@ -6,11 +5,7 @@ local options = {
   rnvimr_hide_gitignore = 1, -- Hide the files included in gitignore
   rnvimr_enable_bw = 1, -- Make Neovim wipe the buffers corresponding to the files deleted by Ranger
   rnvimr_shadow_winblend = 50, -- Add a shadow window, value is equal to 100 will disable shadow
-  rnvimr_ranger_cmd = { 'ranger' }, -- Draw border with both
-  rnvimr_ranger_views = [[
-    [ \	{'minwidth': 90, 'ratio': []}, \	{'minwidth': 90, 'maxwidth': 89, 'ratio': [1,1]}, \	{'maxwidth': 90, 'ratio': [1]} ]
-  ]],
-  -- rnvimr_border_attr = [[{'fg': 14, 'bg': -1}]]
+  rnvimr_ranger_cmd = { "ranger" }, -- Draw border with both
 }
 
 for k, v in pairs(options) do
@@ -18,5 +13,15 @@ for k, v in pairs(options) do
 end
 
 -- Link CursorLine into RnvimrNormal highlight in the Floating window
-vim.cmd[[highlight link RnvimrNormal CursorLine]]
+vim.cmd([[highlight link RnvimrNormal CursorLine]])
 
+vim.cmd([[
+let g:rnvimr_layout = {
+  \ 'relative': 'editor',
+  \ 'width': float2nr(round(0.95 * &columns)),
+  \ 'height': float2nr(round(0.95 * &lines)),
+  \ 'col': float2nr(round(0.025 * &columns)),
+  \ 'row': float2nr(round(0.025 * &lines)),
+  \ 'style': 'minimal'
+  \ }
+]])
