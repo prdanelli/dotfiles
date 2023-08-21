b698fbba3f153be4c5efc4a5796d1d88afdae60b
Paul Danelli <prdanelli@gmail.com>
2023-08-21 21:13:34 +0100

    Fix issue with single Ruby files outside a Git repo

--- nvim/lua/lsp/settings/solargraph.lua
+++ nvim/lua/lsp/settings/solargraph.lua
@@ -1,7 +1,9 @@
+local util = require("lspconfig.util")
+
 return {
   settings = {
     Solargraph = {
-      root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git")(fname) or vim.fn.getcwd()
+      root_dir = util.root_pattern("Gemfile", ".git")(fname) or util.path.dirname(vim.api.nvim_buf_get_name(0)),
     },
   },
 }
