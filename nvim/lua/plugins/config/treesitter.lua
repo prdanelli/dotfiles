b539bab0f508851dfacc0509c2e6094d7cda50d7
Paul Danelli <prdanelli@gmail.com>
2023-08-21 21:14:11 +0100

    Remove keymaps

--- nvim/lua/plugins/config/treesitter.lua
+++ nvim/lua/plugins/config/treesitter.lua
@@ -26,23 +26,6 @@ local languages = {
   "vim",
 }
 
-local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
--- Repeat movement with ; and ,
--- ensure ; goes forward and , goes backward regardless of the last direction
-vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
--- vim way: ; goes to the direction you were moving.
--- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
--- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
--- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-
 configs.setup({
   context_commentstring = {
     enable = true,
