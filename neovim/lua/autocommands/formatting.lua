06d0c96f3924b36a2cf0f7633a46c5b7f5c845e7
Paul Danelli <prdanelli@gmail.com>
2023-11-30 18:20:25 +0000

    Ruby formatters

--- neovim/lua/autocommands/formatting.lua
+++ neovim/lua/autocommands/formatting.lua
@@ -1,14 +1,10 @@
 vim.api.nvim_create_augroup("_formatting", { clear = true })
 
--- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
-  group = highlight_group,
+-- Adjust how text is formatted
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = "_formatting",
   pattern = "*",
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+  end,
 })
-
--- vim.cmd [[highlight IncSearch cterm=reverse gui=reverse]]
