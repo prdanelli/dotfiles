0d2073a504f2ffed45e923b026f22572b6410e53
Paul Danelli <prdanelli@gmail.com>
2024-02-04 17:03:53 +0000

    Delete plugin

--- neovim/lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
--- Vim motion suggestions
-return {
-  "m4xshen/hardtime.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- Add "oil" to the disabled_filetypes
-    disabled_filetypes = require("config.excluded_filetypes"),
-  },
-}
