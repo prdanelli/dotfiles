1279bf08c408c72fed17b8a8f9e4f4799e0232fd
Paul Danelli <prdanelli@gmail.com>
2024-02-04 17:04:09 +0000

    Fix neovim main installer locations

--- zsh/user/install_neovim_head.sh
+++ zsh/user/install_neovim_head.sh
@@ -8,5 +8,5 @@ wget -q -P $DOWNLOADS "https://github.com/neovim/neovim/releases/download/nightl
 xattr -c "$DOWNLOADS/$FILENAME"
 tar xzf "$DOWNLOADS/$FILENAME"
 mv "$TARGET/bin/nvim" ~/.local/bin
-rm -rf $TARGET
-rm $FILENAME
+rm -rf $DOWNLOADS/$TARGET
+rm $DOWNLOADS/$FILENAME
