6c8629f745fcb2d5c00bb7a1749ac3228a5047c5
Paul Danelli <prdanelli@gmail.com>
2024-04-12 13:17:15 +0100

    Update neavim head installer

--- zsh/user/install_neovim_head.sh
+++ zsh/user/install_neovim_head.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-TARGET=nvim-macos
+TARGET=nvim-macos-arm64
 FILENAME="$TARGET.tar.gz"
 DOWNLOADS=$HOME/Downloads
 
 wget -q -P $DOWNLOADS "https://github.com/neovim/neovim/releases/download/nightly/$FILENAME"
 xattr -c "$DOWNLOADS/$FILENAME"
-tar xzf "$DOWNLOADS/$FILENAME"
+tar -xzf "$DOWNLOADS/$FILENAME"
 mv "$TARGET/bin/nvim" ~/.local/bin
 rm -rf $DOWNLOADS/$TARGET
 rm $DOWNLOADS/$FILENAME
