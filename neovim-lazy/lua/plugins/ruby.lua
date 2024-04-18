ee3001902d5328f1765ede8d6010cb4c60497bbb
Paul Danelli <prdanelli@gmail.com>
2024-04-18 13:12:23 +0100

    Remove neotest - it doesn't work and I can't be bothered figuring out why

--- neovim-lazy/lua/plugins/ruby.lua
+++ neovim-lazy/lua/plugins/ruby.lua
@@ -31,57 +31,6 @@ return {
   },
 
   {
-    "nvim-neotest/neotest",
-    optional = false,
-    dependencies = {
-      "olimorris/neotest-rspec",
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      adapters = {
-        ["neotest-rspec"] = {
-          rspec_cmd = function()
-            return vim.tbl_flatten({ "bundle", "exec", "rspec" })
-          end,
-        },
-      },
-    },
-    keys = {
-      {
-        "<leader>ctn",
-        function()
-          require("neotest").run.run()
-        end,
-        desc = "Test: Nearest",
-      },
-      {
-        "<leader>cta",
-        function()
-          require("neotest").run.attach()
-        end,
-        desc = "Test: Attach",
-      },
-      {
-        "<leader>cts",
-        function()
-          require("neotest").run.stop()
-        end,
-        desc = "Test: Stop",
-      },
-      {
-        "<leader>ctf",
-        function()
-          require("neotest").run.run(vim.fn.expand("%"))
-        end,
-        desc = "Test: File",
-      },
-    },
-  },
-
-  {
     "RRethy/nvim-treesitter-endwise",
     event = "InsertEnter",
   },
