5fc8e66cebf8e867d6bcc3eadbc2d1e5bfe43ef8
Paul Danelli <prdanelli@gmail.com>
2024-05-09 10:34:59 +0100

    Switch back to nvim-surround

--- neovim-lazy/lua/plugins/mini-surround.lua
+++ neovim-lazy/lua/plugins/mini-surround.lua
@@ -1,14 +1,10 @@
 return {
-  "echasnovski/mini.surround",
-  opts = {
-    mappings = {
-      add = "sa",
-      delete = "sd",
-      find = "sf",
-      find_left = "sF",
-      highlight = "sh",
-      replace = "sr",
-      update_n_lines = "sn",
-    },
-  },
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end,
 }
