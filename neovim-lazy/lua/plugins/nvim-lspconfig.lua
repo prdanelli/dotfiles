263092ea6f9cf2cb018bc29740de04b7ddf92a3f
Paul Danelli <prdanelli@gmail.com>
2024-05-24 15:57:47 +0100

    Use native quicklist definitions list over telescope which crashes pretty often

--- neovim-lazy/lua/plugins/nvim-lspconfig.lua
+++ neovim-lazy/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    -- options for vim.diagnostic.config()
     diagnostics = {
       underline = true,
       update_in_insert = false,
@@ -9,9 +8,6 @@ return {
         spacing = 4,
         source = "if_many",
         prefix = "●",
-        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-        -- prefix = "icons",
       },
       severity_sort = true,
       signs = {
@@ -23,58 +19,42 @@ return {
         },
       },
     },
-    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-    -- Be aware that you also will need to properly configure your LSP server to
-    -- provide the inlay hints.
     inlay_hints = {
       enabled = true,
     },
-    -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
-    -- Be aware that you also will need to properly configure your LSP server to
-    -- provide the code lenses.
     codelens = {
       enabled = false,
     },
-    -- add any global capabilities here
     capabilities = {},
-    -- options for vim.lsp.buf.format
-    -- `bufnr` and `filter` is handled by the LazyVim formatter,
-    -- but can be also overridden when specified
     format = {
       formatting_options = nil,
       timeout_ms = nil,
     },
-    ---@type lspconfig.options
     servers = {
       lua_ls = {
-        ---@type LazyKeysSpec[]
-        -- keys = {},
         settings = {
           Lua = {
-            workspace = {
-              checkThirdParty = false,
-            },
             codeLens = {
               enable = true,
             },
             completion = {
               callSnippet = "Replace",
             },
+            diagnostics = {
+              globals = { "vim", "LazyVim" },
+            },
+            workspace = {
+              checkThirdParty = false,
+            },
           },
         },
       },
     },
-    -- you can do any additional lsp server setup here
-    -- return true if you don't want this server to be setup with lspconfig
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-    setup = {
-      -- example to setup with typescript.nvim
-      -- tsserver = function(_, opts)
-      --   require("typescript").setup({ server = opts })
-      --   return true
-      -- end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,
-    },
+    setup = {},
   },
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+    keys[#keys + 1] = { "gd", vim.lsp.buf.definition }
+  end,
 }
