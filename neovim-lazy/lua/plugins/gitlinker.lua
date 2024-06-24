e9f95e3c6ed93813c73a2d496e0c93da851bc691
Paul Danelli <prdanelli@gmail.com>
2024-06-24 19:38:12 +0100

    Git link keymap

--- neovim-lazy/lua/plugins/gitlinker.lua
+++ neovim-lazy/lua/plugins/gitlinker.lua
@@ -9,9 +9,9 @@ return {
 
     vim.keymap.set(
       { "n", "v" },
-      "<leader>gl",
+      "<leader>gw",
       "<cmd>lua require('gitlinker').get_buf_range_url('n', { action_callback = require('gitlinker.actions').open_in_browser })<cr>",
-      { desc = "Git Link", silent = true }
+      { desc = "View in [w]ebsite", silent = true }
     )
   end,
 }
