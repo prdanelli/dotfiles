947c9b2e30ec18d7de6b104aee29f76a94eda800
Paul Danelli <prdanelli@gmail.com>
2024-07-23 11:02:51 +0100

    Update for new version

--- neovim-lazy/lua/plugins/which-key.lua
+++ neovim-lazy/lua/plugins/which-key.lua
@@ -1,25 +1,32 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    preset = "classic",
     show_help = true,
-    ignore_missing = false,
     plugins = {
-      marks = false,
-      registers = false,
+      marks = true, -- shows a list of your marks on ' and `
+      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      spelling = {
+        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        suggestions = 20, -- how many suggestions should be shown in the list?
+      },
       presets = {
-        operators = false,
-        motions = false,
+        operators = false, -- adds help for operators like d, y, ...
+        motions = false, -- adds help for motions
         text_objects = false, -- help for text objects triggered after entering an operator
         windows = false, -- default bindings on <c-w>
         nav = true, -- misc bindings to work with windows
+        z = true, -- bindings for folds, spelling and others prefixed with z
+        g = true, -- bindings for prefixed with g
+        o = false,
       },
     },
-    window = {
-      border = "none", -- none, single, double, shadow
-      position = "bottom", -- bottom, top
-      margin = { 0, 0, 0, 0 },
-      padding = { 1, 1, 1, 1 },
-      winblend = 0,
+    win = {
+      no_overlap = true,
+      border = "single", -- none, single, double, shadow
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
     },
     layout = {
       height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -27,12 +34,7 @@ return {
       spacing = 10, -- spacing between columns
       align = "center", -- align columns left, center or right
     },
-    triggers_blacklist = {
-      i = { "j", "k" },
-      v = { "j", "k" },
-      n = { "b", "B", "o", "O", "j", "k", "h", "l" }, -- Ensure special chars aren't slowed down
-    },
-    defaults = {
+    spec = {
       mode = { "n", "v" },
       ["g"] = { name = "+goto" },
       ["gs"] = { name = "+surround" },
@@ -47,6 +49,14 @@ return {
       ["<leader>u"] = { name = "+ui" },
       ["<leader>w"] = { name = "+windows" },
       ["<leader>x"] = { name = "+quickfix" },
+    },
+    icons = {
+      breadcrumb = "» ", -- symbol used in the command line area that shows your active key combo
+      separator = "➜ ", -- symbol used between a key and it's label
+      group = "+ ", -- symbol prepended to a group
+      ellipsis = "…",
+      rules = {},
+      colors = false,
     },
   },
 }
