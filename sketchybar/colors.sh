6a65aee44082c8b5a95b607e9fd4c950146d16cf
Paul Danelli <prdanelli@gmail.com>
2024-12-11 10:50:17 +0000

    Add sketchybar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export BG_LIGHT=0xff353b49
+export BG=0xff2e3440
+export FG=0xffeCEFF4
+export RED=0xffbf616a
+export ORANGE=0xffd08770
+export ORANGE_WASHED=0xffdfac9c
+export YELLOW=0xffebcb8b
+export BLUE=0xff5e81ac
+export LBLUE=0xff6c86a1
+export GREEN=0xffa3be8c
+export CYAN=0xff88c0d0
+export MAGENTA_DARK=0xff9d6b93
+export MAGENTA=0xffb48ead
+export MAGENTA_LIGH=0xffcbb1c7
+export PINK=0xffFFA19F
+export GREY1=0xfff8fafc
+export GREY2=0xfff0f1f4
+export GREY3=0xffeaecf0
+export GREY4=0xffd9dce3
+export GREY5=0xffc4c9d4
+export GREY6=0xffb5bcc9
+export GREY7=0xff929cb0
+export GREY8=0xff8e99ae
+export GREY9=0xff74819a
+export GREY10=0xff616d85
+export GREY11=0xff464f62
+export GREY12=0xff3a4150
+export GREY13=0xff333a47
+export GREY14=0xff242932
+export GREY15=0xff1e222a
+export GREY16=0xff1c1f26
+export GREY17=0xff0f1115
+export GREY18=0xff0d0e11
+export GREY19=0xff020203
+
+export TRANSPARENT=0x00000000
---
+++ sketchybar/plugins/space.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# The $SELECTED variable is available for space components and indicates if
+# the space invoking this script (with name: $NAME) is currently selected:
+# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+
+source "$CONFIG_DIR/colors.sh"
+
+# sketchybar --set "$NAME" background.drawing="$SELECTED"
+# the difference is in this line:
+sketchybar --set "$NAME" background.drawing="$SELECTED" color=$MAGENTA icon.highlight="$SELECTED" icon.highlight_color=$BG
+
+# and mouse events for special mouse-over highlight effect
+case "$SENDER" in
+"mouse.entered")
+  sketchybar --set $NAME icon.highlight=on icon.highlight_color=$MAGENTA
+  ;;
+"mouse.exited" | "mouse.exited.global")
+  sketchybar --set $NAME icon.highlight_color=$MAGENTA icon.highlight=$SELECTED
+  ;;
+"mouse.clicked")
+  # clicked effect
+  sketchybar --set $NAME icon.highlight_color=$MAGENTA label.highlight_color=$MAGENTA
+  sketchybar --set $NAME icon.highlight=$SELECTED label.highlight=$SELECTED
+  ;;
+esac
