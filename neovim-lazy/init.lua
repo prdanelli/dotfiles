eab870bc5c5223bbfafa87b68ec742be4e632c40
Paul Danelli <prdanelli@gmail.com>
2024-12-29 22:30:31 +0000

    Add keymaps/options to lazy

--- neovim-lazy/init.lua
+++ neovim-lazy/init.lua
@@ -1,4 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.help")
+require("config.options")
 require("config.commands")
+require("config.lazy")
+require("config.keymaps")
+require("config.autocmds")
