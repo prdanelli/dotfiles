50456a0b00251a9dd939b75acf785a5db20096e7
Paul Danelli <prdanelli@gmail.com>
2025-01-08 11:36:42 +0000

    Blink config

---
+++ neovim-lazy/lua/plugins/blink.lua
@@ -0,0 +1,53 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = { border = "rounded" },
+      },
+      ghost_text = {
+        enabled = false,
+      },
+      list = {
+        selection = { preselect = true, auto_insert = false },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+          },
+        },
+        border = "rounded",
+      },
+    },
+    keymap = {
+      preset = "enter",
+
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+  },
+}
