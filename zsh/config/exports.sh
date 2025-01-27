c5ff9d935c5f7a69b0f2d0c354fbda363b2bc1fe
Paul Danelli <prdanelli@gmail.com>
2025-01-27 16:46:39 +0000

    Update exports

--- zsh/config/exports.sh
+++ zsh/config/exports.sh
@@ -3,15 +3,16 @@
 ################################################################################
 
 # First items are added first, but referred to last - first in, last out.
-export PATH=/opt/homebrew/opt/postgresql@15/bin:$PATH
-export PATH=/opt/homebrew/opt/yarn:$PATH
-export PATH=/opt/homebrew/opt/python@3.9:$PATH
-export PATH=/opt/homebrew/opt/openssl@3:$PATH
-export PATH=/usr/local/bin:$PATH     # Recommended by brew doctor
-export PATH=/opt/homebrew/sbin:$PATH # Recommended by brew doctor
-export PATH=/opt/homebrew/bin:$PATH  # Brew is first as everything else uses that
+export PATH=/usr/local/bin:$PATH # Recommended by brew doctor
 export PATH=$HOME/.bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
+export PATH=/opt/homebrew/opt/postgresql@15/bin:$PATH
+export PATH=/opt/homebrew/opt/yarn:$PATH
+export PATH=/opt/homebrew/opt/python@3.13:$PATH
+export PATH=/opt/homebrew/opt/openssl@3:$PATH
+export PATH=/opt/homebrew/sbin:$PATH # Recommended by brew doctor
+export PATH=/opt/homebrew/bin:$PATH  # Brew is first as everything else uses that
+export PATH=/opt/homebrew/opt/rustup/bin:$PATH
 
 export LIBRARY_PATH=$LIBRARY_PATH:/opt/homebrew/opt/zstd/lib/ # Fix for MySQL2 gem not compiling
 
