e35da5ef595506f1ade78fa42d44effb32f04fae
Paul Danelli <prdanelli@gmail.com>
2025-01-31 20:52:58 +0000

    Ditch mini surround for nvim surround

--- neovim-kickstart/lua/plugins/mini.lua
+++ neovim-kickstart/lua/plugins/mini.lua
@@ -66,55 +66,6 @@ return {
 
     require("mini.pairs").setup()
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-
-    -- require("mini.surround").setup({
-    --   -- Add custom surroundings to be used on top of builtin ones. For more
-    --   -- information with examples, see `:h MiniSurround.config`.
-    --   custom_surroundings = nil,
-    --
-    --   -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-    --   highlight_duration = 200,
-    --
-    --   -- Module mappings. Use `''` (empty string) to disable one.
-    --   mappings = {
-    --     add = "sa", -- Add surrounding in Normal and Visual modes
-    --     delete = "sd", -- Delete surrounding
-    --     find = "sf", -- Find surrounding (to the right)
-    --     find_left = "sF", -- Find surrounding (to the left)
-    --     highlight = "sh", -- Highlight surrounding
-    --     replace = "sr", -- Replace surrounding
-    --     update_n_lines = "sn", -- Update `n_lines`
-    --
-    --     suffix_last = "", -- Suffix to search with "prev" method
-    --     suffix_next = "", -- Suffix to search with "next" method
-    --   },
-    --
-    --   -- Number of lines within which surrounding is searched
-    --   n_lines = 20,
-    --
-    --   -- Whether to respect selection type:
-    --   -- - Place surroundings on separate lines in linewise mode.
-    --   -- - Place surroundings on each line in blockwise mode.
-    --   respect_selection_type = false,
-    --
-    --   -- How to search for surrounding (first inside current line, then inside
-    --   -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-    --   -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
-    --   -- see `:h MiniSurround.config`.
-    --   search_method = "cover_or_nearest",
-    --
-    --   -- Whether to disable showing non-error feedback
-    --   -- This also affects (purely informational) helper messages shown after
-    --   -- idle time if user input is required.
-    --   silent = false,
-    -- })
-    --
-    -- -- if 'timeoutlen' is low enough to cause occasional usage of |s| key disable it with the following call
-    -- vim.keymap.set({ "n", "x" }, "s", "<Nop>")
+    require("mini.splitjoin").setup()
   end,
 }
--- neovim-kickstart/lua/plugins/nvim-surround.lua
+++ neovim-kickstart/lua/plugins/nvim-surround.lua
@@ -4,7 +4,19 @@ return {
   event = "VeryLazy",
   config = function()
     require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
     })
   end,
 }
