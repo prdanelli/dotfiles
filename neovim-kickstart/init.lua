7feddcd121ab59b0e763addc524a6d18bdb0294e
Paul Danelli <prdanelli@gmail.com>
2025-02-05 10:48:08 +0000

    Move health

--- neovim-kickstart/init.lua
+++ neovim-kickstart/init.lua
@@ -3,7 +3,7 @@ require "config.commands"
 require "config.lazy"
 require "config.keymaps"
 require "config.autocmds"
-require "kickstart.health"
+require "config.health"
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
