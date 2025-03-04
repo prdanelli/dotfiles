048bc6c2138da300a128abccf9bdd8c357f2f1d8
Paul Danelli <prdanelli@gmail.com>
2025-03-04 15:52:24 +0000

    Tweak to use fzf preview

--- zsh/config/fzf.sh
+++ zsh/config/fzf.sh
@@ -10,8 +10,8 @@ export FZF_CTRL_R_OPTS="
 
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'bat -n --color=always {}'
+  --walker-skip .git,node_modules,target,.DS_Store
+  --preview 'fzf-preview.sh {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'
   --height=50%"
 
