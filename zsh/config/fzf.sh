43237613e4fd790da61c76237d33d6203fac545f
Paul Danelli <prdanelli@gmail.com>
2025-03-04 18:54:00 +0000

    Setup fzf to use rc file

--- zsh/config/fzf.sh
+++ zsh/config/fzf.sh
@@ -1,32 +1,31 @@
-eval "$(fzf --zsh)"
-
 # Exports
 
+# Set default config file
+export FZF_DEFAULT_OPTS_FILE=~/.fzfrc
+
+# History
 # CTRL-Y to copy the command into clipboard using pbcopy
 export FZF_CTRL_R_OPTS="
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
   --color header:italic
   --header 'CTRL-Y to copy into clipboard'
-  --height=50%"
+  --height=100%
+  --preview-window=:hidden"
 
+# Files / Directories
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target,.DS_Store
   --preview 'fzf-preview.sh {}'
-  --height=50%"
+  --height=100%"
 
-# Print tree structure in the preview window
-export FZF_ALT_C_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'tree -C {}'"
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow'
 
-export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow '
-
-export FZF_DEFAULT_OPTS="--bind='ctrl-o:become(nvim {})' --preview 'fzf-preview.sh {}' --padding=0% --color='dark,fg:magenta' --layout=reverse --border-label=' Search ' --info=hidden --prompt='▶ ' --pointer='▶' --marker='♡' --padding 0,1 --border --style=full --no-scrollbar --bind 'focus:transform-preview-label:[[ -n {} ]] && printf \" %s \" {}' --color=dark --color 'border:#464f62,label:#6c7a96' --color 'input-border:#464f62,input-label:#ffcccc' --color 'gutter:#353b49' --color 'current-bg:#353b49' --color 'current-hl:#ebcb8b' --color 'list-fg:#6c7a96' --color 'pointer:#ebcb8b'"
-
+################################################################################
 # tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
 # `tm` will allow you to select your tmux session via fzf.
 # `tm irc` will attach to the irc session (if it exists), else it will create it.
+################################################################################
 tm() {
   [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
   if [ $1 ]; then
@@ -36,9 +35,11 @@ tm() {
   session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) && tmux $change -t "$session" || echo "No sessions found."
 }
 
+################################################################################
 # fs [FUZZY PATTERN] - Select selected tmux session
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
+################################################################################
 fs() {
   local session
   session=$(tmux list-sessions -F "#{session_name}" |
@@ -46,7 +47,9 @@ fs() {
     tmux switch-client -t "$session"
 }
 
+################################################################################
 # ftpane - switch pane (@george-b)
+################################################################################
 ftpane() {
   local panes current_window current_pane target target_window target_pane
   panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
@@ -66,17 +69,21 @@ ftpane() {
   fi
 }
 
+################################################################################
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
+################################################################################
 fe() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
+################################################################################
 # Modified version where you can press
 #   - CTRL-O to open with `open` command,
 #   - CTRL-E or Enter key to open with the $EDITOR
+################################################################################
 fo() {
   IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
   key=$(head -1 <<<"$out")
@@ -86,13 +93,17 @@ fo() {
   fi
 }
 
+################################################################################
 # fda - including hidden directories
+################################################################################
 fda() {
   local dir
   dir=$(find ${1:-.} -type d 2>/dev/null | fzf +m) && cd "$dir"
 }
 
+################################################################################
 # fdr - cd to selected parent directory
+################################################################################
 fdr() {
   local declare dirs=()
   get_parent_dirs() {
@@ -107,12 +118,17 @@ fdr() {
   cd "$DIR"
 }
 
+################################################################################
 # fh - repeat history
+################################################################################
 fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
+################################################################################
 # fkill - kill processes - list only the ones you can kill. Modified the earlier script.
+################################################################################
+
 fkill() {
   local pid
   if [ "$UID" != "0" ]; then
@@ -125,3 +141,5 @@ fkill() {
     echo $pid | xargs kill -${1:-9}
   fi
 }
+
+eval "$(fzf --zsh)"
