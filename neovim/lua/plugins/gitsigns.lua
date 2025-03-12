c888c5eed08c0c7f5d12131d054f1e9f7f354a68
Paul Danelli <prdanelli@gmail.com>
2025-03-12 21:53:16 +0000

    Cleaning up plugins

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -12,7 +12,7 @@ return {
       },
       auto_attach = true,
       attach_to_untracked = false,
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
